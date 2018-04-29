257c82b3c80e94f1d2c4b5eb6ee66e39cef0deca
David Brainard <brainard@psych.upenn.edu>
2018-04-29 15:01:01 -0400

    Put back and commented out desinusoiding call

--- Code/Test/aoTestRegister.m
+++ Code/Test/aoTestRegister.m
@@ -125,6 +125,19 @@ sysPara.timePerFrame = (sysPara.vtSync + sysPara.vtBackPorch...
 [desinMovie,imagePara] = aoReadMovie(desinusoidedMovieFile,maxMovieLength);
 actualMovieLength = length(desinMovie);
 
+%% Suggest moving testing of desinusoiding to its own test program.
+%
+% Note: it is trouble if you load the contents of a .mat file directly into a workspace.
+% This might clobber some variables that already exist.  Better to load into a structure
+% and then use that way.  I've made that change in the commented out code
+% below.
+%
+% This code did not make sense to me, because desinusoiding was being
+% applied to an already desinusoided movie.
+%
+% desinTransform = load(desinTransformFile);
+% desinMovies = aoDesinusoid(desinTransform.vertical_fringes_desinusoid_matrix,rawMovies,maxMovieLength);
+
 %% Step 2: Get reference image and set image size
 if (isempty(refImageName))
     refImage = desinMovie(1).cdata;
