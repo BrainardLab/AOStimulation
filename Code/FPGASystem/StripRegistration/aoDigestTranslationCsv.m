b925d706ed91547f5974d949993adebe9e3352ad
David Brainard <brainard@psych.upenn.edu>
2018-05-11 14:10:55 -0400

    Woriking on understanding translation csv file.

--- Code/FPGASystem/StripRegistration/aoDigestTranslationCsv.m
+++ Code/FPGASystem/StripRegistration/aoDigestTranslationCsv.m
@@ -1,7 +1,8 @@
-%function aoDigestTranslationCsv(translationCsvFile)
+function [eyePositionX,eyePositionY] = aoDigestTranslationCsv(translationCsvFile)
 % Read in a magic file that gives the alignment info from Alf's alignment
 %
 % Syntax:
+%    [eyePositionX,eyePositionY] = aoDigestTranslationCsv(translationCsvFile)
 %
 % Description:
 %    Alf's demotion software produces output that gives the row by row
@@ -11,7 +12,7 @@
 %    for each row in each frame.
 %
 % Inputs:
-%    translationCsvFile       - String.  Filename of translation csf file.
+%    translationCsvFile       - String.  Full path to translation csv file
 %
 % Outputs:
 %
@@ -23,130 +24,77 @@
 % History:
 %   05/09/18  dhb, rc, tyh  Started on it.
 
-%% Name the project.
-theProject = 'AOStimulation';
+% Examples:
+%{
+    theTestFile = '/NC_11002_20160405_OD_confocal_0116/NC_11002_20160405_OD_confocal_0116_ref_83_lps_8_lbss_8_sr_n_143_cropped_1_transforms.csv');
+    aoDigestTranslationCsv(theTestFile);
+%}
 
-% hook by hand each time you want to work on this project.
-movieBaseDir = getpref(theProject,'MovieBaseDir');
+%% Define the CSV file
+csvFile = fullfile(translationCsvFile);
 
-% Read file
-%% Define input directory and corresponding files
+%% Read the CSV file
+rawData = csvread(csvFile);
+
+%% Get size of aligned movie workspace
+alignedMovieRows = rawData(1,2);
+alignedMovieCols = rawData(1,4);
+
+%% Get frame by frame data.
+% Comes in groups of three lines per frame
 %
-% Available files
-%   NC_11002_20160405_OD_confocal_0116
-%   NC_11002_20160405_OD_confocal_0136
-%   NC_11002_20160405_OD_confocal_0128
-%   NC_11002_20160405_OD_confocal_0133
-%   NC_11002_20160405_OD_confocal_0124
-testDirectoryName = 'NC_11002_20160405_OD_confocal_0116';
-movieDir = fullfile(movieBaseDir,testDirectoryName);
-
-% Read ALF's output movie
-movieFile = fullfile(movieDir,'NC_11002_20160405_OD_confocal_0116_ref_83_lps_8_lbss_8_sr_n_143_cropped_1.avi');
-
-[theMovie,movieParams] = aoReadMovie(movieFile,0);
-
-% Get the size of the movie
-[s1,s2,s3] = size(theMovie);
-
-% Pick up one frame
-imagePick = theMovie(:,:,1);
-
-% Find the valid image size
-flagFirst = 0;
-for i=2:s1
-    for j=2:s2
-        if (imagePick(i,j)>0 && (flagFirst == 0))
-            firstPoint = [i,j];
-            flagFirst = 1;
-        end
-        if (imagePick(i,j)>0)
-            lastPoint = [i,j];
-            
-        end
-    end
+% Each column of the data is about one row in the input image.
+% Not all rows are aligned, though, and actual number varies
+% frame by frame.
+frameNumberData = rawData(2:end,1);
+frameData = rawData(2:end,2:end);
+[h,w] = size(frameData);
+if (rem(h,3) ~= 0)
+    error('Something unexpected in csv file format');
 end
+nFrames = h/3;
+maxImageRows = w;
 
-imageSize = lastPoint - firstPoint;
-
-% Define the CSV file 
-csvFile = fullfile(movieDir,'NC_11002_20160405_OD_confocal_0116_ref_83_lps_8_lbss_8_transforms.csv');
-
-% Read the CSV file
-csvData1 = csvread(csvFile);
-
-% Pick up the frames
-csvData = csvData1(5:end,:);
-
-% Size the data
-[h,w] = size(csvData);
-w=w-1;
-
-% Get the total number of frame in the data
-nFrame = h/3;
-
-% Sort  the frame in order to compare with our methods
+%% Sort  the frames in order to compare with our methods
+%
 % Frame loop
-for i=1:nFrame
+for ii = 1:nFrames
+    % Get index into the first row for the frame
+    frameStartRowIndex = 1+(ii-1)*3;
+    frameNumber = frameNumberData(frameStartRowIndex);
     
-    % Get the frame index
-    frameIdx = csvData(1+(i-1)*3,1);
+    % Toss out zeros at end of each row
+    theFrameYGlobalOffsetData = frameData(frameStartRowIndex,:);
+    temp = theFrameYGlobalOffsetData(end);
+    while (temp == 0)
+        theFrameYGlobalOffsetData = theFrameYGlobalOffsetData(1:end-1);
+        temp = theFrameYGlobalOffsetData(end);
+    end
+    nAlignedLines = length(theFrameYGlobalOffsetData);
+    theFrameXOffsetData = frameData(frameStartRowIndex+1,1:nAlignedLines);
+    theFrameYOffsetData = frameData(frameStartRowIndex+2,1:nAlignedLines);
     
-    % Get the base move
-    globalMove(1+fix(i/3)) = csvData(1+(i-1)*3,2);
-    
-    % Get the displacement
-    xDisplacement = csvData(2+(i-1)*3,2:end);
-    yDisplacement = globalMove(1+fix(i/3)) + csvData(3+(i-1)*3,2:end);
-    
-    % Sort the frames
-    xDisplacementFrame(:,frameIdx) = xDisplacement;
-    yDisplacementFrame(:,frameIdx) = yDisplacement;
-    
-end
-
-% Analyze the CSV result
-numberOfFrame = nFrame;
-%numberOfFrame = 1;
-
-% Vasualize the result
-figure;hold on
-
-% Frame loop for x-direction displacment
-for i=1:numberOfFrame
-    
-    % Plot
-    if (mod(i,2)==0)
-        plot(1+(i-1)*w:i*w,xDisplacementFrame(:,i),'ro','MarkerSize',2,'MarkerFaceColor','r');
-    else
-        plot(1+(i-1)*w:i*w,xDisplacementFrame(:,i),'go','MarkerSize',2,'MarkerFaceColor','g');
+    % Check that they really are sequential for each frame
+    if (any(diff(theFrameYGlobalOffsetData) ~= 1))
+        fprintf('A line is skipeed for frame %d\n',ii);
     end
     
-    % Limit y axis
-    ylim([-150 150]);
-end
-ylabel('Displacement')
-xlabel('line number');
-title(sprintf('col(x) displacement'));
-hold off
-
-%
-figure;hold on
-
-% Frame loop for y-direction
-for i=1:numberOfFrame
-    
-    % Plot
-    if (mod(i,2)==0)
-        plot(1+(i-1)*w:i*w,yDisplacementFrame(:,i),'ro','MarkerSize',2,'MarkerFaceColor','r');
-    else
-        plot(1+(i-1)*w:i*w,yDisplacementFrame(:,i),'go','MarkerSize',2,'MarkerFaceColor','g');
+    % Get eye position for every aligned line
+    previousGlobalOffset = theFrameYGlobalOffsetData(1)-1;
+    currentLine = 1;
+    for jj = 1:nAlignedLines
+        currentGlobalOffset = theFrameYGlobalOffsetData(jj);
+        while (currentGlobalOffset < previousGlobalOffset + 1)
+            eyePositionY(ii,currentLine) = NaN;
+            eyePositionX(ii,currentLine) = NaN;
+            currentLine = currentLine + 1;
+            currentGlobalOffset = currentGlobalOffset + 1;
+            fprintf('Skipping a line\n');
+        end
+        
+        eyePositionY(ii,currentLine) = theFrameYOffsetData(jj) - currentGlobalOffset;
+        eyePositionX(ii,currentLine) = theFrameXOffsetData(jj);
+        previousGlobalOffset = currentGlobalOffset;
     end
-    
-    % Limit y axis
-    ylim([-150 150]);
 end
-ylabel('Displacement')
-xlabel('line number');
-title(sprintf('row(y) displacement'));
-hold off
+
---
+++ Code/FPGASystem/Test/aoTestDigestTranslationCsv.m
@@ -0,0 +1,123 @@
+% Test our routine for reading a translation csv file.
+%
+% Description:
+%    The Demotion software does strip registration, and it is possible
+%    to get information about what it has done.  We can read that
+%    information from a csv file, which we store in the same directory
+%    as the input and aligned movies.
+%
+%    This program tests our ability to read and make sense of the 
+%    csv file.
+%
+% See also: aoDigestTranslationCsv
+%
+
+% History:
+%  05/10/18 tyh        Started on this.
+%  05/11/18 tyh, dhb   Moved into its own test script.
+
+%% Name the project.
+theProject = 'AOStimulation';
+
+%% Get pointer to to movie base directory, where things live.
+% This is set up by tbUseProject('AOStimulation');
+movieBaseDir = getpref(theProject,'MovieBaseDir');
+
+%% Read input movie file, and the corresponding demotion output
+% Define input directory and corresponding files
+%
+% Available files
+%   NC_11002_20160405_OD_confocal_0116, ref_83_lps_8_lbss_8_sr_n_143_cropped_1
+testDirectoryName = 'NC_11002_20160405_OD_confocal_0116';
+alignedMovieSuffix = 'ref_83_lps_8_lbss_8_sr_n_143_cropped_1';
+
+% Read aligned movie
+movieDir = fullfile(movieBaseDir,testDirectoryName);
+
+% Read aligned movie
+[alignedMovie,alignedMovieParams] = aoReadMovie(fullfile(movieDir,[testDirectoryName '_' alignedMovieSuffix '.avi']),0);
+
+% Read translation csv file
+[eyePositionX,eyePositionY] = aoDigestTranslationCsv(fullfile(movieDir,[testDirectoryName '_' alignedMovieSuffix '_transforms.csv']));
+
+% Get the size of the movie
+[s1,s2,s3] = size(alignedMovie);
+
+% Pick up one frame
+imagePick = alignedMovie(:,:,1);
+
+% Find the valid image size
+flagFirst = 0;
+for i=2:s1
+    for j=2:s2
+        if (imagePick(i,j)>0 && (flagFirst == 0))
+            firstPoint = [i,j];
+            flagFirst = 1;
+        end
+        if (imagePick(i,j)>0)
+            lastPoint = [i,j];
+            
+        end
+    end
+end
+
+imageSize = lastPoint - firstPoint;
+
+%     % Get the base move
+%     globalMove(1+fix(ii/3)) = frameData(1+(ii-1)*3,2);
+%     
+%     % Get the displacement
+%     xDisplacement = frameData(2+(ii-1)*3,2:end);
+%     yDisplacement = globalMove(1+fix(ii/3)) + frameData(3+(ii-1)*3,2:end);
+%     
+%     % Sort the frames
+%     xDisplacementFrame(:,frameIdx) = xDisplacement;
+%     yDisplacementFrame(:,frameIdx) = yDisplacement;
+%     
+% end
+% 
+% % Analyze the CSV result
+% numberOfFrame = nFrames;
+% %numberOfFrame = 1;
+% 
+% % Vasualize the result
+% figure;hold on
+% 
+% % Frame loop for x-direction displacment
+% for ii=1:numberOfFrame
+%     
+%     % Plot
+%     if (mod(ii,2)==0)
+%         plot(1+(ii-1)*maxImageRows:ii*maxImageRows,xDisplacementFrame(:,ii),'ro','MarkerSize',2,'MarkerFaceColor','r');
+%     else
+%         plot(1+(ii-1)*maxImageRows:ii*maxImageRows,xDisplacementFrame(:,ii),'go','MarkerSize',2,'MarkerFaceColor','g');
+%     end
+%     
+%     % Limit y axis
+%     ylim([-150 150]);
+% end
+% ylabel('Displacement')
+% xlabel('line number');
+% title(sprintf('col(x) displacement'));
+% hold off
+% 
+% %
+% figure;hold on
+% 
+% % Frame loop for y-direction
+% for ii=1:numberOfFrame
+%     
+%     % Plot
+%     if (mod(ii,2)==0)
+%         plot(1+(ii-1)*maxImageRows:ii*maxImageRows,yDisplacementFrame(:,ii),'ro','MarkerSize',2,'MarkerFaceColor','r');
+%     else
+%         plot(1+(ii-1)*maxImageRows:ii*maxImageRows,yDisplacementFrame(:,ii),'go','MarkerSize',2,'MarkerFaceColor','g');
+%     end
+%     
+%     % Limit y axis
+%     ylim([-150 150]);
+% end
+% ylabel('Displacement')
+% xlabel('line number');
+% title(sprintf('row(y) displacement'));
+% hold off
